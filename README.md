0c0078b6d368b898d2e9d9a7d0ead45a8934f4c2
lonewolf2580 <linusngwee@yahoo.com>
2021-10-26 13:00:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hb7foa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hb7foa)
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+
+export default class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario kart' },
+    ],
+  };
+  deleteTodo = (id) => {
+    // alert(id);
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
+    });
+  };
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos,
+    });
+  };
+  render() {
+    return (
+      <div className="App container">
+        <h1 class="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
